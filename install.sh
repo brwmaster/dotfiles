4c35f90782b49ea8b94cd36ba86483d86b1e254b
Edwin Boon <edwin.boon@eo.nl>
2024-03-18 08:18:52 +0100

    WIP: Install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/user/bin/env zsh
+
+####################################
+# This script creates symlinks from the home directory to any disired dotfiles in $HOME/dotfiles
+# It also installs MacOS Software
+# It also installs Homebrew Packages and Cask (Apps)
+# It also setup VS Code
+####################################
